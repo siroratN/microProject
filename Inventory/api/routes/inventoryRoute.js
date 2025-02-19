92260d12559aea2c95c1ce8d9480923d06578ad8
Sahatsawat_Fame <108666123+Sundance02@users.noreply.github.com>
2025-02-19 19:44:35 +0700

    fix

--- Inventory/api/routes/inventoryRoute.js
+++ Inventory/api/routes/inventoryRoute.js
@@ -6,13 +6,9 @@ const router = express.Router()
 router.get("/getProducts/:id", inventoryController.getProductsById)
 router.post("/addProduct", inventoryController.addProduct)
 router.post("/updateStock", inventoryController.updateStock)
-<<<<<<< HEAD
-router.get("/getAllProducts", inventoryController.getAllProducts)
-=======
 router.get("/getProducts/", inventoryController.getAllProducts)
 // router.get("/sendAlertMessage", inventoryController.sendAlertMessage)
 
 
 
->>>>>>> 86a73ca5446b82817f46ea017147831a12ad4fe6
 export default router

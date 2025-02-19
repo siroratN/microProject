cd47dd925671178b35bc91470c30d75101ccfe18
Sahatsawat_Fame <108666123+Sundance02@users.noreply.github.com>
2025-02-19 19:41:49 +0700

    'fix'

--- Authen/api/model/Model.js
+++ Authen/api/model/Model.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema(
         password : {type:String, required:true},
         role : {type:String, required:true},
         first_name:{type:String, required:true},
-        last_name:{type:String, required:true}
+        last_name:{type:String, required:true},
+        email:{type:String, required:true},
     }
 )
 
